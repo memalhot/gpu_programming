2824ccd51fb22b783b384ad093df1e3e396f9b41
meeramalhot <meera.malhot@gmail.com>
2025-09-15 15:32:48 +0000

    gitsetup

---
+++ hello.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+__global__ void helloFromGPU(void) {
+    printf("Hello from GPU\n");
+}
+
+int main(void) {
+    printf("Hello from CPU\n");
+    helloFromGPU <<< 1,10 >>>();
+    cudaDeviceSynchronize();
+    cudaDeviceReset();
+}
