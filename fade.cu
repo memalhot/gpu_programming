b5dae19c13baa968d9a4f8d8d509ee6ee688d3ea
meeramalhot <meera.malhot@gmail.com>
2025-09-17 13:44:49 +0000

    finished params

--- fade.cu
+++ fade.cu
@@ -4,11 +4,23 @@ unsigned int idx, v;
 int x = blockDim.x * blockIdx.x + threadIdx.x;
 int y = blockDim.y * blockIdx.y + threatIdx.y;
 
-if ((x >= xmax) || (y >= ymax)) return;
+if ((x >= xmax) || (y >= ymax)) {
+    return;
+}
 
 idx = y * xmax + x;
 v = d_in[idx] * f;
-if (v>255) v=255;
+if (v>255) {
+    v=255;
+}
 d_out[idx]=v;
 
+}
+
+int main(void) {
+    dim3 nblocks(7,3);
+    dim3 nthreads(16, 16);
+    fade <<<nblocks, nthreads>>>(d_in, d_out, f, xmax, ymax); 
+    cudaDeviceSynchronize();
+    cudaDeviceReset();
 }
